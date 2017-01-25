af0b4c9082067c992325d9af615a94f04ae731ba
Justin Smith <smithju071500@cvh.rusd.us>
2017-01-25 10:38:43 -0800

    finished

--- Loops.py
+++ Loops.py
@@ -1,2 +1,8 @@
 """Use a loop to make a turtle draw a shape that is has at least 100 sides and
-that shows symmetry.  The entire shape must fit inside the screen""" 
+that shows symmetry.  The entire shape must fit inside the screen"""
+import turtle
+justin = turtle.Turtle()
+
+for i in range(100):
+    justin.forward(200)
+    justin.left(170)
