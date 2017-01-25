bb738def8aadf9bbd7d87f43b0e7b1caf7577bc8
Justin Smith <smithju071500@cvh.rusd.us>
2017-01-25 11:19:41 -0800

    finished

--- Encrypt.py
+++ Encrypt.py
@@ -1,9 +1,11 @@
 """Write a code that will take (AnyString) and substitute all vowels with
 character's like $#%^&* etc..... but DO NOT USE ' or " as any of your characters."""
-# the example i got online
-def removeVowels(AnyString):
-    output = AnyString[0]
-    for char in AnyString[1:]:
-        if char.lower() not in 'aeiou':
-            output += char
-    return output
+def encryption(e):
+    a = "aeiouAEIOU"
+    b = ""
+    for q in e:
+        if q in a:
+            q = "#"
+        b += q
+    return b
+print(encryption("AP"))
