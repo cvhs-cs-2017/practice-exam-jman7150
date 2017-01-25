26eedcbbe25b342af9398b6258d765061755e793
Justin Smith <smithju071500@cvh.rusd.us>
2017-01-25 11:08:08 -0800

    finished

--- Encrypt.py
+++ Encrypt.py
@@ -1,2 +1,9 @@
 """Write a code that will take (AnyString) and substitute all vowels with
 character's like $#%^&* etc..... but DO NOT USE ' or " as any of your characters."""
+# the example i got online
+def removeVowels(AnyString):
+    output = AnyString[0]
+    for char in AnyString[1:]:
+        if char.lower() not in 'aeiou':
+            output += char
+    return output
--- Functions.py
+++ Functions.py
@@ -1,5 +1,8 @@
 """1. Write a function that will double any integer (n) and return the result"""
-
+def double(x):
+    x = x * 2
+    return x
+print (double(2))
 
 
 
@@ -7,3 +10,9 @@
 """2.  Write a program that will (1) ask the user for an input value, (2) take
 that and double it and  (3) print the result.
 Include necessary print statements and address whitespace issues."""
+def doubledouble(b):
+    print("choose any number")
+    b = int(input())
+    b = b * 2
+    return a
+print(doubledouble(2))
