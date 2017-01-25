1d37d0fe66e861bee52c044a56502e373df90e65
Justin Smith <smithju071500@cvh.rusd.us>
2017-01-25 10:09:53 -0800

    finished

--- Turtle.py
+++ Turtle.py
@@ -1,1 +1,29 @@
 """ Create a Turtle, name it, make it BLUE and draw a Smiley Face"""
+import turtle
+smileyface = turtle.Turtle()
+smileyface.color('Blue')
+
+smileyface.penup()
+smileyface.goto(-75,150)
+smileyface.pendown()
+smileyface.circle(10)
+
+smileyface.penup()
+smileyface.goto(75,150)
+smileyface.pendown()
+smileyface.circle(10)
+
+smileyface.penup()
+smileyface.goto(0,0)
+smileyface.pendown()
+smileyface.circle(100,90)
+
+smileyface.penup()
+smileyface.goto(0,0)
+smileyface.pendown()
+smileyface.circle(-100,90)
+
+smileyface.penup()
+smileyface.goto(0,-100)
+smileyface.pendown()
+smileyface.circle(300)
